6b0690f49195598eaa66a72adc7b61d605c31e79
Bernard2806 <berramuspegonzalez@gmail.com>
2025-03-20 19:20:13 -0300

    Añadir archivos Actividad3.c (Vacio) y Actividad4.c con un programa de conversión de pesos a dólares

---
+++ Clase 2 (19-03)/Actividad4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    const float precioDolarTurista = 1364.03;
+    float pesos, dolares;
+
+    printf("Ingrese la cantidad de pesos a convertir: ");
+    scanf("%f", &pesos);
+
+    dolares = pesos / precioDolarTurista;
+
+    printf("\n");
+
+    printf("La cantidad de dolares es: %.2f", dolares);
+
+    return 0;
+}
