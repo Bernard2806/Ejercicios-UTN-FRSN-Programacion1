70e501bc8af73166dcb5ceb49beccedae0df94af
Bernard2806 <berramuspegonzalez@gmail.com>
2025-03-20 19:08:29 -0300

    Añadir archivo Actividad1.c

---
+++ Clase 2 (19-03)/Actividad1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("Mi Nombre es Bernardo, Mi Dirección es: Luis Mohr 127, Mi Telefono es: 2346-614492 \n\n");
+
+    printf("Mi Nombre es Bernardo, Mi Dirección es: Luis Mohr 127, Mi Telefono es: 2346-614492 \n\n");
+
+    printf("Mi Nombre es Bernardo, Mi Dirección es: Luis Mohr 127, Mi Telefono es: 2346-614492 \n\n");
+
+    return 0;
+}
