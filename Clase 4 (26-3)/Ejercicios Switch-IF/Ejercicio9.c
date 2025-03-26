60adf0c13a795b5f59abb849a1e5bed01e64308d
Bernard2806 <berramuspegonzalez@gmail.com>
2025-03-26 19:48:02 -0300

    Agregar Ejercicio9.c para determinar el momento del día según la hora ingresada

---
+++ Clase 4 (26-3)/Ejercicios Switch-IF/Ejercicio9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int hora;
+
+    printf("Ingrese la hora (entre 0 y 23): ");
+    scanf("%d", &hora);
+
+    prinf("\n");
+    
+    if(hora >= 6 && hora <= 11)
+    {
+        printf("Es de mañana");
+    }
+    else if(hora >= 12 && hora <= 17)
+    {
+        printf("Es de tarde");
+    }
+    else if(hora >= 18 && hora <= 23)
+    {
+        printf("Es de noche");
+    }
+    else if(hora >= 0 && hora <= 5)
+    {
+        printf("Es de madrugada");
+    }
+    else
+    {
+        printf("Hora incorrecta");
+    }
+
+    return 0;
+}
