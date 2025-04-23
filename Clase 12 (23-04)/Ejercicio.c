a2cc4ca22c37c3ce9934fde7bafcbc1d1f5be1e3
Bernardo <berramuspegonzalez@gmail.com>
2025-04-23 20:22:17 -0300

    Refactor Ejercicio: Agreagar funcion para cargar los puntos de equipos.

--- Clase 12 (23-04)/Ejercicio.c
+++ Clase 12 (23-04)/Ejercicio.c
@@ -4,12 +4,15 @@
 // Prototipos de Funciones
 
 void cargarNombreEquipos(char equipos[][25], int cantidadEquipos);
+void cargarPuntosEquipos(int equipos[][4], char equiposNombres[][25], int cantidadEquipos);
 
 int main()
 {
-    char equipos[2][25];
+    char equiposNombres[2][25];
+    int equiposPuntos[2][4];
 
-    cargarNombreEquipos(equipos, 2);
+    cargarNombreEquipos(equiposNombres, 2);
+    cargarPuntosEquipos(equiposPuntos, equiposNombres, 2);
 
     return 0;
 }
@@ -19,7 +22,22 @@ void cargarNombreEquipos(char equipos[][25], int cantidadEquipos)
     for (int i = 0; i < cantidadEquipos; i++)
     {
         printf("Ingrese el nombre del equipo %d: ", i + 1);
-        fflush(stdin);
-        fgets(equipos[i], 25, stdin);
+
+        fflush(stdin);                // Limpiar el buffer de entrada
+        fgets(equipos[i], 25, stdin); // Leer el nombre del equipo
+    }
+}
+
+void cargarPuntosEquipos(int equipos[][4], char equiposNombres[][25], int cantidadEquipos)
+{
+    for (int i = 0; i < cantidadEquipos; i++)
+    {
+        printf("Ingrese los puntos del equipo %s: ", equiposNombres[i]);
+        
+        for (int j = 0; j < 4; j++)
+        {
+            printf("Puntos %d: ", j + 1);
+            scanf("%d", &equipos[i][j]);
+        }
     }
 }
