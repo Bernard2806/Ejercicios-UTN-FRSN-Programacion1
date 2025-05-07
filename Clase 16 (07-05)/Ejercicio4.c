7c90842a9eb508258411df908795664bd39e9c86
Bernardo <berramuspegonzalez@gmail.com>
2025-05-07 19:57:03 -0300

    Añadir Ejercicio 4 de la Clase 16: implementación de funciones para encontrar el máximo en un array utilizando punteros

---
+++ Clase 16 (07-05)/Ejercicio4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+// Prototipo de funciones
+int encontrarMaxArrayConPunterosYSinIndices(int *array, int size);
+void mostrarArray(int arreglo[], int largo);
+
+int main()
+{
+    int array[5] = {10, 20, 30, 40, 50};         // Inicializa un array de enteros
+    int size = sizeof(array) / sizeof(array[0]); // Calcula el tamaño del array
+
+    printf("El array es: ");
+    mostrarArray(array, size); // Muestra el array
+
+    printf("\nEl maximo es: %d\n", encontrarMaxArrayConPunterosYSinIndices(array, size)); // Encuentra y muestra el máximo del array
+
+    return 0;
+}
+
+int encontrarMaxArrayConPunterosYSinIndices(int *array, int size)
+{
+    int max = *array; // Inicializa max con el primer elemento del array
+
+    for (int i = 1; i < size; i++)
+    {
+        if (*(array + i) > max) // Compara el elemento actual con max
+        {
+            max = *(array + i); // Actualiza max si se encuentra un nuevo máximo
+        }
+    }
+
+    return max; // Devuelve el valor máximo encontrado
+}
+
+void mostrarArray(int arreglo[], int largo)
+{
+    for (int i = 0; i < largo; i++)
+    {
+        printf("%d, ", arreglo[i]);
+    }
+}
